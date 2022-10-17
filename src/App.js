cf9df76292e06581d317c61f0530d2ec2c231c55
unknown <gabrielamozer1966@gmai.com>
2022-10-17 19:37:34 +0200

    creating & stop working on addingg widget & where the fuck are styles?

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Chatbot from "react-chatbot-kit";
+import "./App.css";
+import config from "./components/config";
+import ActionProvider from "./components/ActionProvider";
+import MessageParser from "./components/MessageParser";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser}/>
       </header>
     </div>
   );
