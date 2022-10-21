bb56a5b2ad7d9f9d9ea5541cb1dc401e53999711
unknown <gabrielamozer1966@gmai.com>
2022-10-21 17:58:57 +0200

    adding methods from action to parse

--- src/components/MessageParser.js
+++ src/components/MessageParser.js
@@ -16,6 +16,19 @@ class MessageParser {
       this.actionProvider.bye();
     } else if (lowerCaseMessage.includes("manager")) {
       this.actionProvider.karen();
+    } else if (lowerCaseMessage.includes("javascript")) {
+      this.actionProvider.handleJavascriptList();
+    } else if (lowerCaseMessage.includes("data")) {
+      this.actionProvider.handleDataList();
+    } else if (lowerCaseMessage.includes("security")) {
+      this.actionProvider.handleSecurityList();
+    } else if (lowerCaseMessage.includes("Api")) {
+      this.actionProvider.handleAPIList();
+    } else if (
+      lowerCaseMessage.includes("interview") ||
+      lowerCaseMessage.includes("job preparation")
+    ) {
+      this.actionProvider.handleInterviewList();
     } else {
       this.actionProvider.optional();
     }
